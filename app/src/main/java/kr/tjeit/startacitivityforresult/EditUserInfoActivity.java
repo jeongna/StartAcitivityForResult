030be722a2c03ac0f7ba50361bc74634f7837a07
ksh1101 <0solarboy0@naver.com>
2018-12-29 12:42:51 +0900

    이름 / 생년월일 결과 이벤트 분리  - startActivityForResult 참조

--- app/src/main/java/kr/tjeit/startacitivityforresult/EditUserInfoActivity.java
+++ app/src/main/java/kr/tjeit/startacitivityforresult/EditUserInfoActivity.java
@@ -1,14 +1,20 @@
 package kr.tjeit.startacitivityforresult;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class EditUserInfoActivity extends BaseActivity {
+
+    private Button okBtn;
+    private android.widget.EditText nameEdt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_user_info);
+
         bindViews();
         setupEvents();
         setValues();
@@ -16,6 +22,12 @@ public class EditUserInfoActivity extends BaseActivity {
 
     @Override
     public void setupEvents() {
+        okBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
     }
 
@@ -26,6 +38,7 @@ public class EditUserInfoActivity extends BaseActivity {
 
     @Override
     public void bindViews() {
-
+        this.okBtn = (Button) findViewById(R.id.okBtn);
+        this.nameEdt = (EditText) findViewById(R.id.nameEdt);
     }
 }
--- app/src/main/java/kr/tjeit/startacitivityforresult/MainActivity.java
+++ app/src/main/java/kr/tjeit/startacitivityforresult/MainActivity.java
@@ -1,22 +1,43 @@
 package kr.tjeit.startacitivityforresult;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends BaseActivity {
 
+    final static int REQUEST_FOR_USER_NAME = 1000;
+    final static int REQUEST_FOR_USER_BIRTHDAY = 1001;
+
+
     private Button nameInputBtn;
+    private android.widget.TextView birthDayTxt;
+    private Button birthDayBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
         bindViews();
         setupEvents();
         setValues();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_FOR_USER_NAME) {
+//            사용자 이름을 입력받아온 경우
+            Toast.makeText(mContext, "사용자이름", Toast.LENGTH_SHORT).show();
+        }
+        else if (requestCode == REQUEST_FOR_USER_BIRTHDAY)
+            Toast.makeText(mContext, "생년월일", Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -25,7 +46,18 @@ public class MainActivity extends BaseActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(mContext, EditUserInfoActivity.class);
-                startActivity(intent);
+//                startActivity(intent);
+//                입력 결과를 받기 위해 다른 메소드로 intent 실행
+                startActivityForResult(intent, REQUEST_FOR_USER_NAME);
+            }
+
+        });
+        
+        birthDayBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext, BirthDayInputActivity.class);
+                startActivityForResult(intent, REQUEST_FOR_USER_BIRTHDAY);
             }
         });
 
@@ -38,6 +70,10 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void bindViews() {
+
+        this.birthDayBtn = (Button) findViewById(R.id.birthDayBtn);
+        this.birthDayTxt = (TextView) findViewById(R.id.birthDayTxt);
         this.nameInputBtn = (Button) findViewById(R.id.nameInputBtn);
+
     }
 }
