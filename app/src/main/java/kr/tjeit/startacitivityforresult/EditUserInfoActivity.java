81d122d7ebe1190634e5f36f2c9adddb788ac9b1
ksh1101 <0solarboy0@naver.com>
2018-12-29 15:46:55 +0900

    갤러리 화면 호출

--- app/src/main/java/kr/tjeit/startacitivityforresult/EditUserInfoActivity.java
+++ app/src/main/java/kr/tjeit/startacitivityforresult/EditUserInfoActivity.java
@@ -1,5 +1,6 @@
 package kr.tjeit.startacitivityforresult;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -25,6 +26,12 @@ public class EditUserInfoActivity extends BaseActivity {
         okBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.putExtra("이름", nameEdt.getText().toString());
+
+                setResult(RESULT_OK, intent);
+
+                finish();
 
             }
         });
--- app/src/main/java/kr/tjeit/startacitivityforresult/MainActivity.java
+++ app/src/main/java/kr/tjeit/startacitivityforresult/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -12,17 +13,23 @@ public class MainActivity extends BaseActivity {
 
     final static int REQUEST_FOR_USER_NAME = 1000;
     final static int REQUEST_FOR_USER_BIRTHDAY = 1001;
-
+    final static int REQUEST_FOR_PICTURE_GALLERY = 1002;
 
     private Button nameInputBtn;
     private android.widget.TextView birthDayTxt;
     private Button birthDayBtn;
+    private TextView nameTxt;
+    private android.widget.ImageView profileImg;
+    private Button cameraBtn;
+    private Button galleryBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
+
+
         bindViews();
         setupEvents();
         setValues();
@@ -32,11 +39,16 @@ public class MainActivity extends BaseActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
+
         if (requestCode == REQUEST_FOR_USER_NAME) {
 //            사용자 이름을 입력받아온 경우
+
+            if (resultCode == RESULT_OK) {
+                String userName = data.getStringExtra("이름");
+                nameTxt.setText(userName);
+            }
             Toast.makeText(mContext, "사용자이름", Toast.LENGTH_SHORT).show();
-        }
-        else if (requestCode == REQUEST_FOR_USER_BIRTHDAY)
+        } else if (requestCode == REQUEST_FOR_USER_BIRTHDAY)
             Toast.makeText(mContext, "생년월일", Toast.LENGTH_SHORT).show();
     }
 
@@ -61,6 +73,21 @@ public class MainActivity extends BaseActivity {
             }
         });
 
+        cameraBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        galleryBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, REQUEST_FOR_PICTURE_GALLERY);
+            }
+        });
     }
 
     @Override
@@ -70,10 +97,12 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void bindViews() {
-
+        this.galleryBtn = (Button) findViewById(R.id.galleryBtn);
+        this.cameraBtn = (Button) findViewById(R.id.cameraBtn);
+        this.profileImg = (ImageView) findViewById(R.id.profileImg);
         this.birthDayBtn = (Button) findViewById(R.id.birthDayBtn);
         this.birthDayTxt = (TextView) findViewById(R.id.birthDayTxt);
         this.nameInputBtn = (Button) findViewById(R.id.nameInputBtn);
-
+        this.nameTxt = (TextView) findViewById(R.id.nameTxt);
     }
 }
